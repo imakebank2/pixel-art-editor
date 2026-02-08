8a1c9c1ed4ff5ed29f5a2ad71d504c974b035eaf
💤 <ongongfrfr@outlook.com.au>
2026-02-08 13:17:44 +1000

    toolbar

--- src/app.rs
+++ src/app.rs
@@ -38,24 +38,27 @@ impl eframe::App for App {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::SidePanel::right("top_panel").show(ctx, |ui| {
+        egui::SidePanel::left("toolbar").show(ctx, |ui| {
             egui::MenuBar::new().ui(ui, |ui| {
 
             // Light mode/dark mode switch
             egui::widgets::global_theme_preference_switch(ui);
             
             ui.menu_button("File", |ui| {
-                ui.button("Export");
-                ui.button("Import");
-                if ui.button("Quit").clicked() {
-                    ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                if ui.button("Export as png").clicked() {
+
                 }
-            });
-            
-            // ui.add_space(16.0);
-                
-            });
+                if ui.button("Import from png").clicked() {
+
+                }
+            });   
+
+            if ui.button("Help").clicked() {
+
+            }
         });
+        ui.label("tools here");
+    });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
@@ -70,14 +73,7 @@ impl eframe::App for App {
             if ui.button("Increment").clicked() {
                 self.value += 1.0;
             }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
+            
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
             egui::warn_if_debug_build(ui);
             });
