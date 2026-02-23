4f4c448bcdb17be52ac380b897049243eb33ac31
💤 <ongongfrfr@outlook.com.au>
2026-02-23 12:10:33 +1000

    added canvas data types

--- src/app.rs
+++ src/app.rs
@@ -12,6 +12,56 @@ use egui_extras;
 
 const CANVAS_SIZE: i8 = 16;
 
+enum Tool {
+    Pencil,
+    Eraser,
+    Eyedropper,
+    PaintBucket
+}
+
+// Mspaint palatte + transparent: https://lospec.com/palette-list/mspaint-windows-7
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)] // All values are unsigned 32-bit integers, in ARGB format
+pub enum Color {
+    Transparent = 0x00000000,
+    Black       = 0xFF000000,
+    LightGrey   = 0xFF7F7F7F,
+    DarkGrey    = 0xFFC3C3C3,
+    White       = 0xFFFFFFFF,
+    Maroon      = 0xFF880015,
+    Red         = 0xFFED1C24,
+    Brown       = 0xFFB97A57,
+    Orange      = 0xFFFF7F27,
+    Gold        = 0xFFFFC90E,
+    Yellow      = 0xFFFFF200,
+    Beige       = 0xFFEFE4B0,
+    Lime        = 0xFFB5E61D,
+    Green       = 0xFF22B14C,
+    SkyBlue     = 0xFF99D9EA,
+    SteelBlue   = 0xFF7092BE,
+    Cyan        = 0xFF00A2E8,
+    Indigo      = 0xFF3F48CC,
+    Purple      = 0xFFA349A4,
+    Pink        = 0xFFFFAEC9,
+    Lavender    = 0xFFC8BFE7,
+}
+
+impl Color {
+    fn to_color32(self) -> Color32 {
+        // Bit manipulation fuckery that AI wrote
+
+        let v = self as u32;
+
+        let a = ((v >> 24) & 0xFF) as u8;
+        let r = ((v >> 16) & 0xFF) as u8;
+        let g = ((v >> 8)  & 0xFF) as u8;
+        let b = (v & 0xFF) as u8;
+
+        Color32::from_rgba_unmultiplied(r, g, b, a)
+    }
+}
+
 pub struct App {
     label: String,
     value: f32,
@@ -19,6 +69,9 @@ pub struct App {
     eyedropper_image: ImageSource<'static>,
     paintbucket_image: ImageSource<'static>,
     pencil_image: ImageSource<'static>,
+    selected_colour: Color,
+    selected_tool: Tool,
+    pixel_canvas: Vec<Color>
 }
 
 impl App {
@@ -31,7 +84,10 @@ impl App {
             paintbucket_image: include_image!("../assets/paintbucket_tool_button.png"),
             pencil_image: include_image!("../assets/pencil_tool_button.png"),
             label: "Hello World!".to_owned(),
-            value: 2.7
+            value: 2.7,
+            selected_colour: Color::Black,
+            selected_tool: Tool::Pencil,
+            pixel_canvas: vec![Color::Transparent; (CANVAS_SIZE * CANVAS_SIZE) as usize]
          }
     }
 }
